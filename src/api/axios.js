a45fee797c410a35145bb8ee0bd979bee088cecc
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2026-05-15 00:02:57 +0500

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,11 @@
 import axios from "axios"
+
+const rawBaseURL = import.meta.env.VITE_API_URL ?? "http://localhost:5000"
+const baseURL = rawBaseURL.endsWith("/api")
+  ? rawBaseURL
+  : `${rawBaseURL.replace(/\/$/, "")}/api`
+
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL,
   withCredentials: true,
 })
